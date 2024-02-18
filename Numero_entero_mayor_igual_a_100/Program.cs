99d650844a44440186a5699f208d0ac7cf4a5a51
User Name <valdezmoralesalejandramaciel@gmail.com>
2024-02-18 08:26:15 -0500

    Agregar archivos de proyecto.

---
+++ Numero_entero_mayor_igual_a_100/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Número entero mayor o igual a 100");
+Console.WriteLine();
+
+Console.WriteLine("Ingrese la cantidad de números que desea sumar : ");
+
+
+int n = int.Parse(Console.ReadLine());
+
+int sumatoria = 0;
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Ingrese el número {i + 1}:");
+    int numero = int.Parse(Console.ReadLine());
+    sumatoria += numero;
+}
+
+if (sumatoria >= 100)
+{
+    Console.WriteLine($"La sumatoria es {sumatoria} y es mayor o igual a 100.");
+}
+else
+{
+    Console.WriteLine($"La sumatoria es {sumatoria} y es menor que 100.");
+}
